ae35bdfa3a0709b94596399289a2cbe3e575af5b
Mayur Bhatia <23645213+myrse7en@users.noreply.github.com>
2018-12-10 13:05:45 -0500

    New examples

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
 
+let books = [
+  {
+    'title': 'The Grinch',
+    'id': 101,
+    'author': 'Dr. Seuss',
+    'url': 'https://www.imdb.com/title/tt0170016/'
+  },
+  {
+    'title': 'Red Fish Blue Fish',
+    'id': 102,
+    'author': 'Dr. Seuss',
+    'url': 'https://www.amazon.com/Fish-Blue-Myself-Beginner-Books/dp/0756921333'
+  }
+];
+
 class App extends Component {
   render() {
+    // this is JavaScript
+    // const s = 'Hello...'
+
+
+    // below this line, within the return is JSX
     return (
       <div className="App">
-        
+      <h2>{this.props.name}</h2>
+        {/* This is JSX inside the of the return paranthesis with its comment syntax */}
+        {books.map(book =>
+          <div key={book.id}>
+            <span>
+            <a href={book.url} target="_blank">{book.title}</a>
+            </span>
+            <span>{book.author}</span>
+          </div>
+        )}
       </div>
-    );
+    ); // JSX ends with return, JavaScript below
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
+
+
 ReactDOM.render(
-  <App />,
+  <App name={'Dr. Seuss'} />,
   document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 // serviceWorker.unregister();
+
+// stops page from refreshing, however components are still rendered
+if (module.hot) {
+  module.hot.accept();
+}
